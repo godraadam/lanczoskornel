b2666e95009966db7514c7f9219f8eac08f87c8e
Godra Adam <godra.adam@gmail.com>
2021-04-01 11:52:33 +0300

    Generated backend project

---
+++ uniapp/src/test/java/dev/godraadam/uniapp/UniappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.godraadam.uniapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
